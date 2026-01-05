2fc2bd3ffa4972b4aca72b384e13167b92ef2e6e
36bhaskar2004 <bhaskarpaymal@gmail.com>
2026-01-05 13:07:04 +0530

    JinJa and Flash message added

--- form.py
+++ form.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 
 app=Flask(__name__)
+app.secret_key="hfoiufhqeuf"
 
 @app.route("/", methods=['GET', 'POST'])
-def start():
+def main():
     if request.method == 'POST':
         form = request.form
-        return form
+        flash("Your form submited")
+        return render_template('form2.html') 
+    return render_template("main.html")
+
+@app.route("/form")
+def start():
     return render_template('form2.html')
 
 if __name__ == "__main__":
