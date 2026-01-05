aeaaf17cbf3b2bfef14e21f7e63fe29c0efa7a88
36bhaskar2004 <bhaskarpaymal@gmail.com>
2026-01-05 14:42:25 +0530

    redirect & dynamic fuction code added

--- form.py
+++ form.py
@@ -1,15 +1,33 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect,url_for
 
+#create variable for flask app
 app=Flask(__name__)
 app.secret_key="hfoiufhqeuf"
 
+#route the page
 @app.route("/", methods=['GET', 'POST'])
 def main():
+    #Check the requests 
     if request.method == 'POST':
-        form = request.form
-        flash("Your form submited")
-        return render_template('form2.html') 
+
+        #take the values from the HTML 
+        num1 = request.form.get("num1")
+        num2 = request.form.get("num2")
+        
+        #open page by direct route name
+        #return redirect("/sum"+"/"+num1+"/"+num2)
+
+        #open page by route function()
+        return redirect(url_for("sum", int1 = num1, int2 = num2))
     return render_template("main.html")
+
+@app.route("/sum/<int:int1>/<int:int2>")
+def sum(int1, int2):
+    sum_ = int1 + int2
+
+    #flash message
+    flash(f"Sum: {sum_}")
+    return render_template("form2.html")
 
 @app.route("/form")
 def start():
@@ -17,3 +35,4 @@ def start():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
