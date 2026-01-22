47298477366762c6b708da2766395254887d45dd
36bhaskar2004 <bhaskarpaymal@gmail.com>
2026-01-22 10:16:07 +0530

    contact form

--- contact/main.py
+++ contact/main.py
@@ -1,5 +1,5 @@
 #Flask required moduls
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request,g, session, redirect
 #firebase required library
 import pyrebase
 #firebase configuration
@@ -29,19 +29,50 @@ def main():
         Name = request.form
         db.child("All Data").push(Name)
         return render_template("form.html")
+    return render_template("main.html")
+
+#form route
+@app.route("/form", methods=["GET","POST"])
+def form():
+    if request.method == "POST":
+        Name = request.form
+        db.child("All Data").push(Name)
+        return render_template("form.html")
     return render_template("form.html")
 
 #admin route
 @app.route("/admin", methods=["GET", "POST"])
 def admin():
-    All_data = db.child("All Data").get()
-    return render_template("admin.html", data = All_data)
+    if 'user' in session:
+        All_data = db.child("All Data").get()
+        return render_template("admin.html", data = All_data)
+    return redirect("/login")
 
 #respons route
 @app.route("/response/<string:pushkey>", methods=["GET", "POST"])
 def res(pushkey):
-    res_data = dict(db.child("All Data").child(pushkey).get().val())
-    return render_template("response.html", pushkey = pushkey, res_data = res_data)
+    if 'user' in session:
+        res_data = dict(db.child("All Data").child(pushkey).get().val())
+        return render_template("response.html", pushkey = pushkey, res_data = res_data)
+    return redirect("/login")
+
+#login route for admin
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        session.pop("user", None)
+        if request.form.get("pass") == 'password' and request.form.get("uname") == "Bhaskar":
+            session['user'] = request.form.get('uname')
+            return redirect("/admin")
+    if 'user' in session:
+        return redirect("/admin")
+    return render_template("login.html")
+
+#logout form admin
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect("/login")
 
 #flask app running
 if __name__ == "__main__":
